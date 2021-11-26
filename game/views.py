1da010879b829bdf3da698767a719adeb84ffeb7
可 路 <1300876003@qq.com>
2021-11-27 01:31:49 +0800

    first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    line1 = '<h1 style="text-align: center">术士之战</h1>'
+    line2 = '<img src="https://img2.baidu.com/it/u=1984058067,3319783488&fm=26&fmt=auto" />'
+    return HttpResponse(line1 + line2)
